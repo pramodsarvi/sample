a2e01af4146a65d3c95bc61d537e9b5d47a4b1f9
Pramod Kumar S Sarvi <pramodkumarssarvi@gmail.com>
2021-11-30 18:17:55 +0530

    changes 4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000;
+var port = normalizePort(process.env.PORT || '8081');
 
 app.get('/', (req, res) => {
   res.send('<h2>Hello World!</h2>')
